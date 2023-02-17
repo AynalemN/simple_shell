811e3e7fe94adf7f8439e8167fa0648e466d8b6e
AynalemN <abebeayni@gmail.com>
2023-02-17 07:59:17 -0800

    betty fixed

--- shell.c
+++ shell.c
@@ -3,20 +3,20 @@
 /**
  * main - display prompt and wait for user to enter a command
  * and display an error if EOF
+ * also check the buffer allocated
  * Return: Always 0
  */
-int main()
+int main(void)
 {
 	char *command = NULL;
 	size_t leng = 0;
 
 	while (1)
 	{
-			printf("$ ");
-			getline(&command, &leng, stdin);
-			printf("%s" , command);
-			//checking allocated buffer memory
-			printf("The length of the buffer allocated: %zu\n", leng);
+		printf("$ ");
+		getline(&command, &leng, stdin);
+		printf("%s", command);
+		printf("The length of the buffer allocated: %zu\n", leng);
 	}
 	free(command);
 	return (0);
