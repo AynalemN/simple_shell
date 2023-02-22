07e7eb124346c61e73cd965a9d4bf25a0d710104
AynalemN <abebeayni@gmail.com>
2023-02-22 04:20:54 -0800

    execute by path

--- getpath.c
+++ getpath.c
@@ -1,20 +1,25 @@
 #include "shell.h"
 /**
+ * executeBypath- getting and executing command by path
+ * @argv: the string commands
+ * Return: Always 0
  */
 int executeBypath(char **argv)
 {
 	pid_t pid = fork();
-	
+
 	if (pid == -1)
 	{
-		perror("ERROR pid");
-                return (EXIT_FAILURE);
+		perror("ERROR pid\n");
+		return (EXIT_FAILURE);
 	}
 	else if (pid == 0)
 	{
 		if (execve(argv[0], argv, NULL) == -1)
+		{
 			perror("./shell");
-                        return (EXIT_FAILURE);
+			return (EXIT_FAILURE);
+		}
 	}
 	else
 	{
