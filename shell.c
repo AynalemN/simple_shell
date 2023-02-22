1a478d5284fac21fb206dbb4ea4b6f32d64c8718
Aynalem A. Nigussie <abebeayni@gmail.com>
2023-02-22 19:37:20 +0300

    shell main is updated

--- shell.c
+++ shell.c
@@ -1,21 +1,44 @@
 #include "shell.h"
-/**
- * main - display prompt and wait for user to enter a command,
- * display an error if EOF and execute the program
- * Return: Always 0
- */
-int main(void)
-{
-	while (true)
-	{
-		int argc = 0;
-		char *delim = " \n";
-		char *command = readInput();
-		char **argv = tokenizes(&argc, command, delim);
 
-		executeBypath(argv);
-		freeArray(argv);
-		free(command);
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
-	return (0);
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
