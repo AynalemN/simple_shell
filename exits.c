b7beeb61aa7710b2ca57e753e5e31cd4688eda51
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 09:34:53 +0300

    header updated _strchr

--- exits.c
+++ exits.c
@@ -64,11 +64,14 @@ char *_strncat(char *dest, char *src, int n)
 }
 
 /**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
+ * _strchr - locates a character in a string
+ *
+ * @s: the string to be parsed
+ * @c: the character to look for
+ *
+ * Return: (s) a pointer to the memory area s
  */
+
 char *_strchr(char *s, char c)
 {
 	do {
