f35b9c9100701e00ed80466df733de1e3b5480b4
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 09:49:03 +0300

    header updated for set_alias

--- history_aliance.c
+++ history_aliance.c
@@ -43,11 +43,13 @@ int unset_alias(info_t *info, char *str)
 
 /**
  * set_alias - sets alias to string
+ *
  * @info: parameter struct
  * @str: the string alias
  *
  * Return: Always 0 on success, 1 on error
  */
+
 int set_alias(info_t *info, char *str)
 {
 	char *p;
