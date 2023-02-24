2699a811dd045cbdd893ae02f6a00511def328dc
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 09:51:32 +0300

    header updated for read_history

--- history.c
+++ history.c
@@ -58,10 +58,12 @@ int write_history(info_t *info)
 
 /**
  * read_history - reads history from file
+ *
  * @info: the parameter struct
  *
  * Return: histcount on success, 0 otherwise
  */
+
 int read_history(info_t *info)
 {
 	int i, last = 0, linecount = 0;
