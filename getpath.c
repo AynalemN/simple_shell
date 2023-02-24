885f6d4039414b748bddf0fb4bf0792593eff848
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 09:46:40 +0300

    header updated for _setenv

--- getpath.c
+++ getpath.c
@@ -58,12 +58,15 @@ int _unsetenv(info_t *info, char *var)
 /**
  * _setenv - Initialize a new environment variable,
  *             or modify an existing one
+ *
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  * @var: the string env var property
  * @value: the string env var value
+ *
  *  Return: Always 0
  */
+
 int _setenv(info_t *info, char *var, char *value)
 {
 	char *buf = NULL;
