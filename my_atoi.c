bac521118ce011bf41666416501a7b46bf1ffcfe
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 10:03:52 +0300

    header updated for _atoi

--- my_atoi.c
+++ my_atoi.c
@@ -48,7 +48,9 @@ int _isalpha(int c)
 
 /**
  * _atoi - converts a string to an integer
+ *
  * @s: the string to be converted
+ *
  * Return: 0 if no numbers in string, converted number otherwise
  */
 
