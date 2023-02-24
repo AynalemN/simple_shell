c285c16e429bc911cd5c6bb733f1cf90eea9c2b3
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 09:42:28 +0300

    header updated for siginHadler

--- getLine.c
+++ getLine.c
@@ -163,10 +163,12 @@ int _getline(info_t *info, char **ptr, size_t *length)
 
 /**
  * sigintHandler - blocks ctrl-C
+ *
  * @sig_num: the signal number
  *
  * Return: void
  */
+
 void sigintHandler(__attribute__((unused))int sig_num)
 {
 	_puts("\n");
