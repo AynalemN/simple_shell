2ef7b360038df6984786536013176782e09e3dbb
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 09:12:41 +0300

    header updated for _mycd

--- builtin.c
+++ builtin.c
@@ -33,10 +33,13 @@ int _myexit(info_t *info)
 
 /**
  * _mycd - changes the current directory of the process
+ *
  * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
+ *			constant function prototype.
+ *
  * Return: Always 0
  */
+
 int _mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
