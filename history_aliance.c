fbb107a4b76b2772adf65d99baaa7b4c914df668
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 09:49:38 +0300

    header updated for _myalias

--- history_aliance.c
+++ history_aliance.c
@@ -91,10 +91,13 @@ int print_alias(list_t *node)
 
 /**
  * _myalias - mimics the alias builtin (man alias)
+ *
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
+ *
  *  Return: Always 0
  */
+
 int _myalias(info_t *info)
 {
 	int i = 0;
