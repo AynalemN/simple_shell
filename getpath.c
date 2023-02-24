c1ea480ea04af3c0e98eb6c0e71596693d3f4d84
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 09:45:31 +0300

    header updated for get_environ

--- getpath.c
+++ getpath.c
@@ -2,10 +2,13 @@
 
 /**
  * get_environ - returns the string array copy of our environ
+ *
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
+ *
  * Return: Always 0
  */
+
 char **get_environ(info_t *info)
 {
 	if (!info->environ || info->env_changed)
