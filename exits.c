9c7684c08b8aed095778eef20229b11133a131eb
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 09:34:23 +0300

    header updated _strncat

--- exits.c
+++ exits.c
@@ -34,12 +34,15 @@ char *_strncpy(char *dest, char *src, int n)
 }
 
 /**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
+ * _strncat - concatenates two strings
+ *
+ * @dest: the first string
+ * @src: the second string
+ * @n: the amount of bytes to be maximally used
+ *
+ * Return: the concatenated string
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
