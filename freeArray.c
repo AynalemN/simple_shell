8b8024cc21ebea74c85dc01771970c24385569aa
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 09:36:05 +0300

    header updated for bfree

--- freeArray.c
+++ freeArray.c
@@ -2,10 +2,12 @@
 
 /**
  * bfree - frees a pointer and NULLs the address
+ *
  * @ptr: address of the pointer to free
  *
  * Return: 1 if freed, otherwise 0.
  */
+
 int bfree(void **ptr)
 {
 	if (ptr && *ptr)
