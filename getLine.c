23ea2de3147b4a71fe1e5e442d6274183cc74047
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 09:40:04 +0300

    unwanted section removed

--- getLine.c
+++ getLine.c
@@ -15,28 +15,26 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 	ssize_t r = 0;
 	size_t len_p = 0;
 
-	if (!*len) /* if nothing left in the buffer, fill it */
+	if (!*len)
 	{
-		/*bfree((void **)info->cmd_buf);*/
 		free(*buf);
 		*buf = NULL;
 		signal(SIGINT, sigintHandler);
-#if USE_GETLINE
+		#if USE_GETLINE
 		r = getline(buf, &len_p, stdin);
-#else
+		#else
 		r = _getline(info, buf, &len_p);
-#endif
+		#endif
 		if (r > 0)
 		{
 			if ((*buf)[r - 1] == '\n')
 			{
-				(*buf)[r - 1] = '\0'; /* remove trailing newline */
+				(*buf)[r - 1] = '\0'; 
 				r--;
 			}
 			info->linecount_flag = 1;
 			remove_comments(*buf);
 			build_history_list(info, *buf, info->histcount++);
-			/* if (_strchr(*buf, ';')) is this a command chain? */
 			{
 				*len = r;
 				info->cmd_buf = buf;
