f7fd94e1863272b627d3f4dc50225d7a61a4a6f6
Melaku Demeke <melaku.demeke789@gmail.com>
2023-02-24 09:41:35 +0300

    header updated for read_buf

--- getLine.c
+++ getLine.c
@@ -93,12 +93,14 @@ ssize_t get_input(info_t *info)
 
 /**
  * read_buf - reads a buffer
+ *
  * @info: parameter struct
  * @buf: buffer
  * @i: size
  *
  * Return: r
  */
+
 ssize_t read_buf(info_t *info, char *buf, size_t *i)
 {
 	ssize_t r = 0;
